4b235dd48c224dfe3aec8714a28eb21fdfe9bceb
noa1231 <nakayuu1759@gmail.com>
2022-04-19 21:39:20 +0900

    todo

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -5,25 +5,20 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>COACHTECH</title>
-  <style>
-    .conteiner{
-      background-color: #2d197c;
-      height: 100vh;
-      width: 100vw;
-    }
-  </style>
+  <link rel="stylesheet" href="css/reset.css" />
+  <link rel="stylesheet" href="css/style.css" />
 </head>
 <body>
-  <din class="conteiner">
-    <div class="card"></div>
+  <div class="conteiner">
+    <div class="card">
       <h2>Todo List</h2>
       <form action="/todo/create" method="post">
         @csrf
-        <input type="text" name="content">
-        <input type="submit" value="追加">
         @foreach ($errors->all() as $error)
             <li>{{$error}}</li>
         @endforeach
+        <input type="text" name="content" class="top_input">
+        <input type="submit" value="追加" class="top_button">
       </form>
       <table>
         <tr>
@@ -41,22 +36,22 @@
           <form action="{{ route('todo.update',[$item->content]) }}" method="post">
             @csrf
           <input type="hidden" name="id" value="{{$item->id}}">
-          <input type="text" name="content" value="{{$item->content}}">
+          <input type="text" name="content" value="{{$item->content}}" class="content_input">
           </td>
           <td>
-          <button type="submit">更新</button>
+          <button type="submit" class="update_button">更新</button>
           </form>
           </td>
           <td>
           <form action="{{ route('todo.delete',['id' => $item->id]) }}" method="post">
             @csrf
-          <button type="submit">削除</button>
+          <button type="submit" class="delete_button">削除</button>
           </form>
           </td>
         </tr>
         @endforeach
       </table>
     </div>
-  </din>
+  </div>
 </body>
 </html>
